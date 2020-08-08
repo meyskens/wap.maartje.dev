09d6f1b4c39d2f6bb96ed1ae7b5903fec048b94c
Maartje Eyskens <maartje@eyskens.me>
2020-08-08 19:20:40 +0200

    Handle DLs myself
    Signed-off-by: Maartje Eyskens <maartje@eyskens.me>

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"os"
+	"path"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
@@ -10,7 +12,7 @@ import (
 func main() {
 	e := echo.New()
 	e.GET("/*", serveWML)
-	e.Static("/dl/*", "./static/dl")
+	e.GET("/dl/*", serveDL)
 
 	e.Start(":80")
 }
@@ -25,4 +27,25 @@ func serveWML(c echo.Context) error {
 	}
 
 	return c.Stream(http.StatusOK, "text/vnd.wap.wml", f)
+}
+
+func serveDL(c echo.Context) error {
+	c.Path()
+	_, file := path.Split(c.Path())
+	f, err := os.Open("./static/dl/" + file)
+
+	if err == os.ErrNotExist {
+		c.String(http.StatusNotFound, "")
+	} else if err != nil {
+		c.String(http.StatusInternalServerError, "")
+	}
+
+	// Go is incorrect when judging Java files
+	// this overrides that
+	contentType := "binary/octet-stream"
+	if strings.HasSuffix(file, ".jar") {
+		contentType = "application/java-archive"
+	}
+
+	return c.Stream(http.StatusOK, contentType, f)
 }
