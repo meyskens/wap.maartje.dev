85078bb4a7db8b46caebde9778a2d5ead04ce494
Maartje Eyskens <maartje@eyskens.me>
2021-12-28 12:03:49 +0100

    Use WP Photon

--- nws.go
+++ nws.go
@@ -81,11 +81,11 @@ func serveNewsItem(c echo.Context) error {
 	}
 
 	if article.Image != nil {
-		item.ImageURL = fmt.Sprintf("http://photon.innovatete.ch/%s?w=100", strings.Replace(article.Image.URL, "https://", "", -1))
+		item.ImageURL = fmt.Sprintf("http://i0.wp.com/%s?w=100", strings.Replace(article.Image.URL, "https://", "", -1))
 	} else if len(article.Enclosures) > 0 {
 		for _, enclosure := range article.Enclosures {
 			if strings.HasPrefix(enclosure.Type, "image") {
-				item.ImageURL = fmt.Sprintf("http://photon.innovatete.ch/%s?w=100", strings.Replace(enclosure.URL, "https://", "", -1))
+				item.ImageURL = fmt.Sprintf("http://i0.wp.com/%s?w=100", strings.Replace(enclosure.URL, "https://", "", -1))
 				break
 			}
 		}
